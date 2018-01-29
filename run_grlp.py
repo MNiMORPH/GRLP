383e6a3bae0c6d81ee44e67a39c6a32408a8a295
Andy Wickert <andy@northernwidget.com>
2018-01-29 01:24:12 -0600

    plot: analytical matches numerical

--- run_grlp.py
+++ run_grlp.py
@@ -5,7 +5,7 @@ import grlp
 reload(grlp)
 
 S0 = 1E-2
-P_xB = 0.8
+P_xB = 0.1
 
 lp = grlp.LongProfile()
 self = lp
@@ -14,7 +14,7 @@ lp.basic_constants()
 lp.bedload_lumped_constants()
 lp.set_hydrologic_constants()
 
-lp.set_x(dx=1000, nx=50, x0=2000)
+lp.set_x(dx=1000, nx=50, x0=20000)
 lp.set_z(S0=-S0)
 lp.set_A(k_xA=1.)
 lp.set_Q(q_R=0.01, A_R=1E5)
@@ -22,12 +22,20 @@ lp.set_B(k_xB=10./np.max(lp.x**P_xB), P_xB=P_xB)
 #lp.set_B(k_xB=100., P_xB=0.)
 lp.set_uplift_rate(0)
 lp.set_niter()
-Qs0 = lp.k_Qs * lp.Q[0] * (10*S0)**(7/6.)
+Qs0 = lp.k_Qs * lp.Q[0] * (100*S0)**(7/6.)
 lp.set_Qs_input_upstream(Qs0)
 lp.set_bcr_Dirichlet(0)
 #lp.set_uplift_rate(0.01/3.15E7)
-lp.evolve_threshold_width_river(100, 1E8)
+lp.evolve_threshold_width_river(50, 1E8)
+lp.analytical_threshold_width(P_xB=P_xB)
 lp.compute_Q_s()
 
+
 plt.ion()
-plt.plot(self.x/1000., self.z)
+plt.figure(figsize=(12,6))
+plt.plot(lp.x/1000., lp.z, '0.6', linewidth=6)
+plt.plot(lp.x/1000., lp.zanalytical, 'k', linewidth=2)
+plt.xlabel('Downstream distance [km]', fontsize=26)
+plt.ylabel('Elevation [m]', fontsize=26)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.tight_layout()

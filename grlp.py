00ee099cce109c13bc3012c11f8d3ac2ed14e318
Andy Wickert <andy@northernwidget.com>
2018-03-23 13:48:36 -0500

    intermittency

--- grlp.py
+++ grlp.py
@@ -330,7 +330,7 @@ class LongProfile(object):
     def compute_Q_s(self):
         self.S = np.abs( (self.z_ext[2:] - self.z_ext[:-2]) / (2*self.dx) ) \
                  / self.sinuosity
-        self.Q_s = self.k_Qs * self.Q * self.S**(7/6.)
+        self.Q_s = self.k_Qs * self.intermittency * self.Q * self.S**(7/6.)
 
     def slope_area(self, verbose=False):
         self.S = np.abs( (self.z_ext[2:] - self.z_ext[:-2]) / (2*self.dx) )

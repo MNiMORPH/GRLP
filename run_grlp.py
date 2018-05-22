de932f93809f7f74c6935e0b4602bf9c32227224
Andy Wickert <andy@northernwidget.com>
2018-05-22 11:04:36 -0600

    updated to still work

--- run_grlp.py
+++ run_grlp.py
@@ -5,14 +5,16 @@ import grlp
 reload(grlp)
 
 S0 = 1E-2
-P_xB = 0.1
+P_xB = 0.8
 
 lp = grlp.LongProfile()
 self = lp
 
+lp.set_intermittency(0.01)
 lp.basic_constants()
 lp.bedload_lumped_constants()
 lp.set_hydrologic_constants()
+lp.set_base_level(0.)
 
 lp.set_x(dx=1000, nx=50, x0=20000)
 lp.set_z(S0=-S0)
@@ -22,11 +24,10 @@ lp.set_B(k_xB=10./np.max(lp.x**P_xB), P_xB=P_xB)
 #lp.set_B(k_xB=100., P_xB=0.)
 lp.set_uplift_rate(0)
 lp.set_niter()
-Qs0 = lp.k_Qs * lp.Q[0] * (100*S0)**(7/6.)
+Qs0 = lp.k_Qs * lp.Q[0] * (10*S0)**(7/6.)
 lp.set_Qs_input_upstream(Qs0)
-lp.set_bcr_Dirichlet(0)
 #lp.set_uplift_rate(0.01/3.15E7)
-lp.evolve_threshold_width_river(50, 1E8)
+lp.evolve_threshold_width_river(5, 1E10)
 lp.analytical_threshold_width(P_xB=P_xB)
 lp.compute_Q_s()
 

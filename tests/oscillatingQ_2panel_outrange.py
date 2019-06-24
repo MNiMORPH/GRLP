eee726ac36a350aed065a5e9664836e1e34f97eb
Andy Wickert <andy@northernwidget.com>
2019-06-24 09:11:37 -0500

    Additions from November/December 2018

--- tests/oscillatingQ_2panel_outrange.py
+++ tests/oscillatingQ_2panel_outrange.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import os
 
 import grlp
 reload(grlp)
@@ -9,15 +10,21 @@ P_xB = 0.2
 #z1 = 1000
 z1 = 0
 
+outroot = '/home/awickert/Dropbox/Papers/InProgress/GRLP_oscillator/OscillatingQ_2panel_outrange/'
+
 zall_max = []
 zall_min = []
-for Qperiod_kyr in [20, 40, 100]:
+for Qperiod_kyr in [20, 40, 100, 400]:
+
+  print Qperiod_kyr, 'kyr cycle'
 
   Qamp = 0.5
-  dt = 3.15E7 * 1E3
   Qperiod = Qperiod_kyr * 1000. * 3.15E7
   #nt = 2 * int(np.ceil(Qperiod/dt))
-  nt = int(np.ceil(Qperiod*5/dt))+1
+  dt = 3.15E7 * 1E3 / 2.
+  #dt = Qperiod/40.
+  nt = 201
+  #nt = int(np.ceil(Qperiod*5/dt))+1
   Bmax = 100.
 
   lp = grlp.LongProfile()
@@ -30,7 +37,7 @@ for Qperiod_kyr in [20, 40, 100]:
   lp.bedload_lumped_constants()
   lp.set_hydrologic_constants()
 
-  lp.set_x(dx=1000, nx=60, x0=4E4)
+  lp.set_x(dx=1000, nx=61, x0=4E4)
   lp.set_z(S0=-S0, z1=z1)
   lp.set_A(k_xA=1.)
   lp.set_Q(q_R=0.01, A_R=1E4)
@@ -112,32 +119,92 @@ for Qperiod_kyr in [20, 40, 100]:
   zall_max.append( np.max(zall[-int(round(len(zall)*2/5.)):], axis=0) )
   zall_min.append( np.min(zall[-int(round(len(zall)*2/5.)):], axis=0) )
   
+  print "plotting"
+  plt.ioff()
+  for i in np.hstack((np.zeros(5), range(nt))).astype(int):
+      i = int(i)
+      #if i%1 == 0:
+      if t[i]/3.15E10 <= 100.:
+          print i,
+          fig = plt.figure(figsize=(12,9))
+          ax1 = plt.subplot(211)
+          ax2 = plt.subplot(212)
+          ax1.set_xlabel('Downstream distance [km]', fontsize=26)
+          ax1.set_ylabel('Elevation [m]', fontsize=26)
+          ax2.set_xlabel('Time [kyr]', fontsize=26)
+          ax2.set_ylabel('Discharge multiplier [-]', fontsize=26)
+          ax1.tick_params(axis='both', which='major', labelsize=16)
+          ax2.tick_params(axis='both', which='major', labelsize=16)
+          ax1.plot(lp.x/1000., z0, '0.7', linewidth=6)
+          ax1.plot(lp.x/1000., zall[i], '0.3', linewidth=2)
+          ax2.plot(t/3.15E10, Qmult, 'k-', linewidth=3)
+          ax2.plot(t[i]/3.15E10, Qmult[i], 'ko')
+          ax1.set_ylim((0, 500))
+          ax1.set_xlim((40, 100))
+          #ax2.set_xlim((0, t[-1]/3.15E10))
+          ax2.set_xlim((0, 100))
+          fig.tight_layout()
+          try:
+              os.mkdir(outroot+'Animate_'+'%03d' %Qperiod_kyr+'kyr')
+          except:
+              pass
+          plt.savefig(outroot+'Animate_'+'%03d' %Qperiod_kyr+'kyr/'+'LP_'+'%06d' %(np.round(t[i]/3.15E7))+'.png')
+          plt.close()
+  
+  print ""
+
+# long profile with envelope of total change
+
+print "DC+"
+# DC shift
+lp.Q = Q0 * np.max(Qmult)
+lp.set_Qs_input_upstream(Qs0)
+lp.evolve_threshold_width_river(1000, 3.15E7 * 1000.)
+ztmp1 = lp.z.copy()
+print "DC-"
+lp.Q = Q0 * np.min(Qmult)
+lp.set_Qs_input_upstream(Qs0)
+lp.evolve_threshold_width_river(1000, 3.15E7 * 1000.)
+ztmp2 = lp.z.copy()
+
+zall_max.append( np.max((ztmp1, ztmp2), axis=0) )
+zall_min.append( np.min((ztmp1, ztmp2), axis=0) )
+
 
 plt.figure()
-plt.plot(lp.x, zall_max[0]-zall_min[0], '.7', linewidth=4, label='20 kyr cycle')
-plt.plot(lp.x, zall_max[1]-zall_min[1], '.4', linewidth=4, label='40 kyr cycle')
-plt.plot(lp.x, zall_max[2]-zall_min[2], '.1', linewidth=4, label='100 kyr cycle')
+plt.plot(lp.x/1000., zall_max[3]-zall_min[3], '.0', linewidth=4, label='400 kyr cycle')
+plt.plot(lp.x/1000., zall_max[2]-zall_min[2], '.25', linewidth=4, label='100 kyr cycle')
+plt.plot(lp.x/1000., zall_max[1]-zall_min[1], '.5', linewidth=4, label='40 kyr cycle')
+plt.plot(lp.x/1000., zall_max[0]-zall_min[0], '.75', linewidth=4, label='20 kyr cycle')
+plt.xlim(40,100)
 plt.xlabel('Downstream distance [km]', fontsize=16)
 plt.ylabel('Maximum elevation difference\nAcross one cycle [m]', fontsize=16)
 #plt.title( str(int(np.round(Qperiod/(1000*3.15E7)))) + ' kyr cycle', fontsize=16, fontweight='bold')
 plt.legend()
 plt.tight_layout()
+#for extension in ['svg', 'eps', 'pdf', 'png']:
+#  plt.savefig(outroot + 'Line_Departures_From_Mean.' + extension)
 
 #colors = ['r', 'g', 'b']
-colors = ['.2', '.5', '.8']
+#colors = ['.2', '.5', '.8']
+#colors = ['0', '.2', '.4', '.6', '.8'][::-1]
+colors = ['0', '0', '.25', '.5', '.75'][::-1]
 #alphas = [.2, .5, .9]
-alphas = [1,1,1]
+alphas = [1,1,1,1,1]
 #alphas = [.5,.5,.5]
-labels = ['20 kyr cycle', '40 kyr cycle', '100 kyr cycle']
+labels = ['20 kyr cycle', '40 kyr cycle', '100 kyr cycle', '400 kyr cycle', 'DC']
 
 plt.figure()
-for i in [2, 1, 0]:
-  plt.fill_between(lp.x, zall_max[i] - np.mean((zall_max[i], zall_min[i]), axis=0), zall_min[i] - np.mean((zall_max[i], zall_min[i]), axis=0), color=colors[i], linewidth=1, alpha=alphas[i], label=labels[i])
+for i in [3, 2, 1, 0]:
+  plt.fill_between(lp.x/1000., zall_max[i] - np.mean((zall_max[i], zall_min[i]), axis=0), zall_min[i] - np.mean((zall_max[i], zall_min[i]), axis=0), color=colors[i], linewidth=1, alpha=alphas[i], label=labels[i])
 plt.xlabel('Downstream distance [km]', fontsize=16)
-plt.ylabel('Maximum elevation difference\nAcross one cycle [m]', fontsize=16)
+plt.ylabel('Envelope of departures\nfrom mean elevation [m]', fontsize=16)
+plt.xlim(40,100)
 #plt.title( str(int(np.round(Qperiod/(1000*3.15E7)))) + ' kyr cycle', fontsize=16, fontweight='bold')
 plt.legend()
 plt.tight_layout()
+#for extension in ['svg', 'eps', 'pdf', 'png']:
+#  plt.savefig(outroot + 'Envelope_Departures_From_Mean.' + extension)
 
 
   

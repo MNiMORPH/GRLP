31a8bbcbdda679f9f1883db89ad22beabd28942c
Andy Wickert <andy@northernwidget.com>
2020-04-10 10:35:35 -0500

    v1.2.3: Now I know how __init__ import works!

--- grlp/__init__.py
+++ grlp/__init__.py
@@ -1,5 +1,7 @@
-from . import grlp
-
-__all__ = [
-    'grlp'
-]
+# module will import as grlp (folder name)
+# This sets which objects, functions, etc. are exposed
+# e.g., 
+# from .grlp import LongProfile
+# I will just expose all of them until I start to have too many
+# or want to restrict the UI
+from .grlp import *
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="GRLP",
-    version="1.2.2",
+    version="1.2.3",
     author="Andrew D. Wickert",
     author_email="awickert@umn.edu",
     description="Evolves gravel-bed river long profiles",

cd00d0286f307922f69cd780208114d0dfc46fad
Andy Wickert <andy@northernwidget.com>
2020-10-10 12:04:33 -0500

    1.4.1: open Firefox upon publication

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="GRLP",
-    version="1.4.0",
+    version="1.4.1",
     author="Andrew D. Wickert",
     author_email="awickert@umn.edu",
     description="Evolves gravel-bed river long profiles",

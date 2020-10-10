52ce90adbaffc0eb5f2ad03c3a9a2d2200c9f5ec
Andy Wickert <andy@northernwidget.com>
2020-10-10 11:50:03 -0500

    Clean dist dir prior to new build

--- build_check_pyPI.sh
+++ build_check_pyPI.sh
@@ -1,3 +1,11 @@
 #! /bin/sh
-python setup.py sdist bdist_wheel
+
+# Clean old files in order to not get "400 Client Error: File already exists."
+rm dist/*.tar.gz
+rm dist/*.whl
+
+# Build the new distribution
+python3 setup.py sdist bdist_wheel
+
+# Check the new distribution for a successful build
 twine check dist/*

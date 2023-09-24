05cd1be78e2b60e76144570b29a39f25e63fbaf8
awickert <andy@northernwidget.com>
2023-09-24 10:03:23 -0500

    Stride across upseg (inner) and downseg (outer) when setting x_ext. This ensures that all downsegs are updated from nan even after fixing x_ext to no longer accidentally be a list of "arrays" that actually all just point to the same place in memory. I predict that fixing this and the n*[array] problem in z will solve the major remaining issue in the 5-segment example. Furthermore, I predict that this x-update will fix strangeness that would have occurred due to variable dx not being recognized. This update will also be important for Q. That the code runs even with Q_ext[-1] being nan now in places seems quite problematic, though could be innocently due to where I use downseg.Q[0] instead of Q_ext[-1].
    On this note: at some point, I may need to dump the `_ext` variables and just calculate on the fly using `Q, x, z` from neighboring linked objects. The `_ext` variables take up memory and add clear failure points. However, they are useful to update duirng the iterations.

--- grlp/grlp.py
+++ grlp/grlp.py
@@ -1462,19 +1462,31 @@ class Network(object):
         The final downstream segment has x_ext[-1] for the lateral position
         of the base-level cell.
         """
+        
+        # Order:
+        # Inner (close to each other): upstream.
+        # Outer (strides from each other): downstream.
+        # Right now, this is moot: Downstream = 1
         for lp in self.list_of_LongProfile_objects:
             _idx = 0
             # SET UPSTREAM BOUNDARIES: INTERNAL
-            for upseg_ID in lp.upstream_segment_IDs:
-                upseg = self.list_of_LongProfile_objects[upseg_ID]
-                lp.x_ext[_idx][0] = upseg.x[-1]
-                _idx += 1
+            # Here, max so the downstream-most segment gets looped through
+            # too, even if it has no downseg ID
+            for i_downseg in range(np.max((1, len(lp.downstream_segment_IDs)))):
+                for upseg_ID in lp.upstream_segment_IDs:
+                    upseg = self.list_of_LongProfile_objects[upseg_ID]
+                    lp.x_ext[_idx][0] = upseg.x[-1]
+                    _idx += 1
             # SET DOWNSTREAM BOUNDARIES: INTERNAL
             _idx = 0
             for downseg_ID in lp.downstream_segment_IDs:
+                # For each downstream ID, must update for each upstream
+                # ID
                 downseg = self.list_of_LongProfile_objects[downseg_ID]
-                lp.x_ext[_idx][-1] = downseg.x[0]
-                _idx += 1
+                # Min = 1 so downseg still updated for headwaters segments
+                for i_upseg in range(np.max((1, len(lp.upstream_segment_IDs)))):
+                    lp.x_ext[_idx][-1] = downseg.x[0]
+                    _idx += 1
 
     def update_x_ext_external_upstream(self):
         """

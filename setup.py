3f578e2ff498caeec5efb01b8bb5fbec4e9833ea
awickert <andy@northernwidget.com>
2024-08-17 11:32:27 +0200

    Increment version to 2.0.0-beta.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="GRLP",
-    version="2.0.0-alpha",
+    version="2.0.0-beta",
     author="Andrew D. Wickert",
     author_email="awickert@umn.edu",
     description="Evolves gravel-bed river long profiles",

c780bc4aaa1efb16c04ad2aa375ffb87ca4a4406
Andy Wickert <andy@northernwidget.com>
2025-08-19 15:12:54 +0200

    Update version information to 2.0.0 ahead of release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="GRLP",
-    version="2.0.0-beta",
+    version="2.0.0",
     author="Andrew D. Wickert",
     author_email="awickert@umn.edu",
     description="Evolves gravel-bed river long profiles",

41397fa26f497f8acae062ffda754f521a858910
Andy Wickert <andy@northernwidget.com>
2025-08-19 12:00:16 +0200

    Update for GRLP2, Python3, and running on the CLI.

--- examples/base_level_fall_transience.py
+++ examples/base_level_fall_transience.py
@@ -2,10 +2,10 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
-plt.ion()
+#plt.ion()
 
 import grlp
-reload(grlp)
+#reload(grlp)
 
 S0 = 0.015
 P_xB = 0.2
@@ -30,7 +30,7 @@ lp.set_z(S0=-S0, z1=z1)
 lp.set_A(k_xA=1.)
 lp.set_Q(k_xQ=1.433776163432246e-05, P_xQ=7/4.*0.7)
 lp.set_B(k_xB=Bmax/np.max(lp.x**P_xB), P_xB=P_xB)
-lp.set_niter()
+lp.set_niter(3)
 lp.set_z_bl(z1)
 Qs0 = lp.k_Qs * lp.Q[0] * S0**(7/6.)
 lp.set_Qs_input_upstream(Qs0)
@@ -40,12 +40,14 @@ ax1 = fig.add_subplot(1,1,1)
 plt.xlabel('Downstream distance [km]', fontsize=14, fontweight='bold')
 plt.ylabel('Elevation [m]', fontsize=14, fontweight='bold')
 plt.tight_layout()
+plt.show(block=False)
 
 # Starting case
 U = 0.
 lp.set_uplift_rate(U/3.15E7)
 lp.evolve_threshold_width_river(10, 1E14)
 ax1.plot(lp.x/1000., lp.z - lp.z[0] + 500, color='.5', linewidth=3)
+plt.draw()
 
 # Transient
 U = 1E-3
@@ -53,8 +55,10 @@ lp.set_uplift_rate(U/3.15E7)
 for i in range(10):
     lp.evolve_threshold_width_river(1, 3E11)
     ax1.plot(lp.x/1000., lp.z - lp.z[0] + 500, color='.5', linewidth=1)
+    plt.draw()
 
 # New equilibrium
 lp.evolve_threshold_width_river(1, 1E14)
 ax1.plot(lp.x/1000., lp.z - lp.z[0] + 500, color='0', linewidth=3)
+plt.show()
 

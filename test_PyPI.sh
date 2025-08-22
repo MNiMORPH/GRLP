bc7674bd9a08a4c4fdca243c2fc469072755721f
Andy Wickert <andy@northernwidget.com>
2025-08-22 11:57:05 +0200

    Streamline upload: just testpypy wihtout URL.

--- test_PyPI.sh
+++ test_PyPI.sh
@@ -1,3 +1,10 @@
 #! /bin/sh
-twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+# Old way
+# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+# As of August 19th, 2025
+twine upload --repository testpypi dist/*
+
+# Either way, test it
 firefox https://test.pypi.org/project/GRLP/
